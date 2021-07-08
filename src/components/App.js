caf81cada8530dee28f1323d80302f2a03cae9de
Vlad Safin <djutsu17@gmail.com>
2021-07-08 22:24:48 +0300

    add persistant store

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import "modern-normalize/modern-normalize.css";
 import s from "./App.module.css";
 import Section from "./Section";
@@ -6,21 +5,21 @@ import Form from "./Form";
 import Contacts from "./Contacts";
 import ContactsFilter from "./Contacts/ContactsFilter";
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <Section title={"Phonebook"}>
-          <Form />
-        </Section>
+const App = () => {
+  return (
+    <>
+      <Section title={"Phonebook"}>
+        <Form />
+      </Section>
 
-        <Section title={"Contacts"}>
-          <div className={s.container}>
-            <ContactsFilter />
-            <Contacts />
-          </div>
-        </Section>
-      </>
-    );
-  }
-}
+      <Section title={"Contacts"}>
+        <div className={s.container}>
+          <ContactsFilter />
+          <Contacts />
+        </div>
+      </Section>
+    </>
+  );
+};
+
+export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,41 @@
 import contactsReducer from "./contactsReducer";
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const middleware = [...getDefaultMiddleware(), logger];
+const contactsPersistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["filter"],
+};
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
 
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
   },
   middleware,
   devTools: process.env.NODE_ENV === "development",
 });
 
-export default store;
+const persistor = persistStore(store);
+
+export default { store, persistor };
